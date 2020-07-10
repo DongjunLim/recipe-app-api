eb73474113a09f5fa282bd50b0f70879386b98f6
DongjunLim <lacuna7304@gmail.com>
2020-07-10 17:51:32 +0900

    updated code style

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -38,7 +38,10 @@ class PublicUserApiTests(TestCase):
 
     def test_user_exists(self):
         """ Test creating a user that already exists fails. """
-        payload = {'email': 'test@gmail.com', 'password': 'testpass', 'name': 'test name'}
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'testpass',
+            'name': 'test name'}
         create_user(**payload)
 
         res = self.client.post(CREATE_USER_URL, payload)
@@ -47,7 +50,10 @@ class PublicUserApiTests(TestCase):
 
     def test_password_too_short(self):
         """ Test that the password must be more than 5 chracters. """
-        payload = {'email': 'test@gmail.com', 'password': 'pw', 'name': 'test name'}
+        payload = {
+            'email': 'test@gmail.com',
+            'password': 'pw',
+            'name': 'test name'}
         res = self.client.post(CREATE_USER_URL, payload)
 
         self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
